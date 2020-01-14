a8b1547006c89333c9785632f4dec043abe6c5a1
takatsugu.ishikawa <takatsugu.ishikawa@rakuten.com>
2020-01-14 22:31:47 +0900

    add grid sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,25 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'my-ag-grid-sample';
   columnDefs = [
-    {headerName: 'Make', field: 'make'},
-    {headerName: 'Model', field: 'model'},
-    {headerName: 'Price', field: 'price', editable: true}
+    {headerName: 'Country', field: 'country', width: 120, hide: true, rowGroup: true},
+    {headerName: 'Year', field: 'year', width: 90, hide: true, rowGroup: true},
+    {headerName: 'Sport', field: 'sport', width: 110},
+    {headerName: 'Athlete', field: 'athlete', width: 200},
+    {headerName: 'Gold', field: 'gold', width: 100},
+    {headerName: 'Silver', field: 'silver', width: 100},
+    {headerName: 'Bronze', field: 'bronze', width: 100},
+    {headerName: 'Total', field: 'total', width: 100},
+    {headerName: 'Age', field: 'age', width: 90},
+    {headerName: 'Date', field: 'date', width: 110}
   ];
 
   rowData = [];
 
   ngOnInit() {
-    fetch('https://api.myjson.com/bins/15psn9')
+    fetch('assets/sample.json')
       .then(result => result.json())
       .then(rowData => this.rowData = rowData);
   }
